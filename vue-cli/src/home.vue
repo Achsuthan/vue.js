3ab301792af2346868441d59adc1d97c57b51dfa
Achsuthan <achsuthan@icloud.comS>
2019-10-14 11:15:11 +0530

    1. Sample component file added
    >>> ach

---
+++ vue-cli/src/home.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <p> Current status : {{status}}</p>
+</div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            status: "Critical"
+        }
+    }
+}
+</script>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './home.vue'
+
+Vue.component('Home', Home)
 
 new Vue({
   el: '#app',
